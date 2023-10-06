66794e7374d663aadce1c67b077afe088eac5314
Simran <simrangami07@gmail.com>
2023-10-06 17:52:26 +0545

    Updates

---
+++ precache-manifest.ee5087b3ac2e4e571e5b4ef4a8ec0772.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c292ffe1acf017fbf44da230b73904c6",
+    "url": "/News-App-React-Redux/index.html"
+  },
+  {
+    "revision": "5bfae66b27f68c892828",
+    "url": "/News-App-React-Redux/static/css/main.264eda6d.chunk.css"
+  },
+  {
+    "revision": "192cf4c4c683342487fe",
+    "url": "/News-App-React-Redux/static/js/2.b1dd1bfb.chunk.js"
+  },
+  {
+    "revision": "50b01b64aedfea5f3a01b438bb59e228",
+    "url": "/News-App-React-Redux/static/js/2.b1dd1bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c0877ceb69afab3662",
+    "url": "/News-App-React-Redux/static/js/3.ac57ea6d.chunk.js"
+  },
+  {
+    "revision": "5bfae66b27f68c892828",
+    "url": "/News-App-React-Redux/static/js/main.ffae68a6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/News-App-React-Redux/static/js/main.ffae68a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f58028dc8716f7b4fe7",
+    "url": "/News-App-React-Redux/static/js/runtime-main.17470d0a.js"
+  }
+]);
